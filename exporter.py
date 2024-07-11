ec35b72bf7d10ccbe30a92db2f5350a86a2dff22
operator <operator@dave.cfm.ehu.es>
2024-07-11 13:37:45 +0200

    Fetch the inlet temperature with ipmitool
    We use Python's subprocess module to execute shell commands:
    https://www.datacamp.com/tutorial/python-subprocess

---
+++ exporter.py
@@ -0,0 +1,24 @@
+"""
+Prometheus exporter to collect the inlet temperature of the host
+using the ipmitool command 
+"""
+
+from subprocess import Popen, PIPE
+
+"""
+Fetch the inlet temp
+"""
+
+IPMI_TEMP_SENSOR="System Temp"
+
+system_temp=Popen(["/usr/bin/ipmitool", "sdr", "get", IPMI_TEMP_SENSOR], stdout=PIPE)
+sensor_reading=Popen(["/usr/bin/grep", "Sensor Reading"], stdin=system_temp.stdout, stdout=PIPE, universal_newlines=True)
+inlet_temp=Popen(["/usr/bin/awk", "{print $4}"], stdin=sensor_reading.stdout, stdout=PIPE, universal_newlines=True)
+
+output,errors=inlet_temp.communicate()
+
+system_temp.kill()
+sensor_reading.kill()
+inlet_temp.kill()
+
+print(output)
