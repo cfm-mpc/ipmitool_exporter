4e5562ae40331a0004c9a1255e613c691f8a9bf3
operator <operator@dave.cfm.ehu.es>
2024-07-11 14:00:03 +0200

    Fetch metrics in an infinite loop

--- exporter.py
+++ exporter.py
@@ -5,20 +5,31 @@ using the ipmitool command
 
 from subprocess import Popen, PIPE
 
-"""
-Fetch the inlet temp
-"""
+def fetch():
+    """
+    Fetch the inlet temp
+    """
 
-IPMI_TEMP_SENSOR="System Temp"
+    IPMI_TEMP_SENSOR="System Temp"
 
-system_temp=Popen(["/usr/bin/ipmitool", "sdr", "get", IPMI_TEMP_SENSOR], stdout=PIPE)
-sensor_reading=Popen(["/usr/bin/grep", "Sensor Reading"], stdin=system_temp.stdout, stdout=PIPE, universal_newlines=True)
-inlet_temp=Popen(["/usr/bin/awk", "{print $4}"], stdin=sensor_reading.stdout, stdout=PIPE, universal_newlines=True)
+    system_temp=Popen(["/usr/bin/ipmitool", "sdr", "get", IPMI_TEMP_SENSOR], stdout=PIPE)
+    sensor_reading=Popen(["/usr/bin/grep", "Sensor Reading"], stdin=system_temp.stdout, stdout=PIPE, universal_newlines=True)
+    inlet_temp=Popen(["/usr/bin/awk", "{print $4}"], stdin=sensor_reading.stdout, stdout=PIPE, universal_newlines=True)
 
-output,errors=inlet_temp.communicate()
+    output,errors=inlet_temp.communicate()
 
-system_temp.kill()
-sensor_reading.kill()
-inlet_temp.kill()
+    system_temp.kill()
+    sensor_reading.kill()
+    inlet_temp.kill()
 
-print(output)
+    print(output)
+
+def run_metrics():
+    """
+    Run the metrics in a loop
+    """
+
+    while True:
+        fetch()
+
+run_metrics()
