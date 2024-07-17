d86292d1abd3d28916450b561d4c8045ca85a0e8
Irene Azaceta <irene.azaceta@ehu.eus>
2024-07-17 14:39:40 +0200

    Fetch the inlet temperature executing shell commands

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Prometheus exporter to collect the inlet temperature of the server using the `ip
 # Requirements
 
 - Go
+
+# Instructions
+
+- Test:
+```bash
+[root@dave ipmitool_exporter]# go run main.go
+33
+```
--- main.go
+++ main.go
@@ -1,2 +1,33 @@
 /* Prometheus exporter to collect the inlet temperature of the host
 using the ipmitool command */
+
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+func fetch() string {
+	
+	/* Fetch the inlet temperature running shell commands */
+
+	var IPMI_TEMP_SENSOR string = "System Temp"
+
+	sensor := exec.Command("/usr/bin/ipmitool", "sdr", "get", IPMI_TEMP_SENSOR)
+	output,_ := sensor.Output() 
+
+	re,_ := regexp.Compile("Sensor Reading.*") //Regex to match
+	inlet_temp:=strings.Fields(re.FindString( string(output) ))[3]
+
+	return inlet_temp
+
+}
+
+func main() {
+
+	fmt.Println( fetch() )
+
+}
