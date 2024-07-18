929726320bf0cdac54a42541b6b0a06a802ac48a
operator <operator@dave.cfm.ehu.es>
2024-07-18 10:42:53 +0200

    Start a http server to expose the metrics
    I tried exporting the metrics, but it shows "my_inprogress_request 0"
    Something doesn't work.

--- README.md
+++ README.md
@@ -20,8 +20,24 @@ Prometheus exporter to collect the inlet temperature of the server using the `ip
 [root@dave ipmitool_exporter] go get github.com/prometheus/client_golang/prometheus/promhttp
 ```
 
-- Test:
+- Run the exporter:
 ```bash
 [root@dave ipmitool_exporter] go run main.go
-33
+```
+
+- View the metrics:
+```bash
+[root@dave ~] curl -L http://localhost:8000/metrics
+# HELP go_threads Number of OS threads created.
+# TYPE go_threads gauge
+go_threads 9
+# HELP my_inprogress_request Inlet Temperature
+# TYPE my_inprogress_request gauge
+my_inprogress_request 0
+# HELP process_cpu_seconds_total Total user and system CPU time spent in seconds.
+# TYPE process_cpu_seconds_total counter
+process_cpu_seconds_total 0
+# HELP process_max_fds Maximum number of open file descriptors.
+# TYPE process_max_fds gauge
+process_max_fds 262144
 ```
--- main.go
+++ main.go
@@ -4,13 +4,17 @@ using the ipmitool command */
 package main
 
 import (
-	"fmt"
 	"os/exec"
 	"regexp"
 	"strings"
+	"strconv"
+	"log"
+	"net/http"	
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func fetch() string {
+func fetch() float64 {
 	
 	/* Fetch the inlet temperature running shell commands */
 
@@ -19,15 +23,30 @@ func fetch() string {
 	sensor := exec.Command("/usr/bin/ipmitool", "sdr", "get", IPMI_TEMP_SENSOR)
 	output,_ := sensor.Output() 
 
-	re,_ := regexp.Compile("Sensor Reading.*") //Regex to match
-	inlet_temp:=strings.Fields(re.FindString( string(output) ))[3]
-
+	re,_ := regexp.Compile("Sensor Reading.*") // regex to match
+	inlet := strings.Fields(re.FindString( string(output) ))[3]
+	inlet_temp,_ := strconv.ParseFloat(inlet, 64) // convert into float
+	
 	return inlet_temp
 
 }
 
 func main() {
 
-	fmt.Println( fetch() )
+	// Create and register the metrics
+	inlet_temperature := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "my_inprogress_request",
+		Help: "Inlet Temperature",
+	})
+	prometheus.MustRegister(inlet_temperature)
+
+	// Expose the metrics
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":8000", nil))
+	
+	// Populate the metrics
+	for true {
+		inlet_temperature.Set(fetch())
+	}
 
 }
